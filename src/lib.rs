28c04065e77140c3a1f31283fb00076d77b76a91
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-07-07 16:32:22 -0300

    changed tree structs to use Rc and RefCell
    as the structs were, we would not have been able to do much given the restrictions of rust, so we need to use Rc and RefCell to check borrow rules at runtime

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,59 @@
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
+
+type NodeParent<T> = Weak<RefCell<Node<T>>>;
+type NodeChildren<T> = Vec<Rc<RefCell<Node<T>>>>;
+type NodeSiblings<T> = Vec<Weak<RefCell<Node<T>>>>;
+
 pub struct Node<T> {
     value: T,
-    parent: Option<Box<Node<T>>>,
-    children: Option<Box<Vec<Node<T>>>>,
+    parent: Option<NodeParent<T>>,
+    children: Option<NodeChildren<T>>,
+    siblings: Option<NodeSiblings<T>>,
+}
+type TreeNode<T> = Rc<RefCell<Node<T>>>;
+
+impl<T> Node<T> {
+    fn new(
+        value: T,
+        children: Option<NodeChildren<T>>,
+        siblings: Option<NodeSiblings<T>>,
+        parent: Option<NodeParent<T>>,
+    ) -> TreeNode<T> {
+        Rc::new(RefCell::new(Node {
+            children,
+            parent,
+            siblings,
+            value,
+        }))
+    }
 }
 
-pub struct MerkleTree<T> {
-    tree: Node<T>,
+type Hash = [u8; 64];
+
+type HashTree = TreeNode<Hash>;
+
+pub struct MerkleTree {
+    tree: HashTree,
+    leaves: Vec<TreeNode<Hash>>,
+    root_hash: Hash,
 }
 
-impl<T> MerkleTree<T> {
-    //todo
-    fn create_tree(data: Vec<T>) -> Node<T> {}
+// creates an alias for the trait AsRef<[u8]>, so that we don't have to write every time
+trait Data: AsRef<[u8]> {}
+impl<T: AsRef<[u8]>> Data for T {}
+type Nodes = Vec<HashTree>;
+
+impl MerkleTree {
+    fn create<T: Data>(data: Vec<T>) -> Self {
+        todo!()
+    }
 }
 
-impl<T> From<Vec<T>> for MerkleTree<T> {
+impl<T: Data> From<Vec<T>> for MerkleTree {
     fn from(value: Vec<T>) -> Self {
-        Self {
-            tree: MerkleTree::create_tree(value),
-        }
+        MerkleTree::create(value)
     }
 }
